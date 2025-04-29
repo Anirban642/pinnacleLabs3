1322b323a4bbb8cec19b22ac2db0dbd5f884c6e7
Anirban Das <anirbandas64237@gmail.com>
2025-04-29 11:56:43 +0530

    added everything

---
+++ script.js
@@ -0,0 +1,6 @@
+// Form submission handling
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for contacting me! I will get back to you soon.');
+    this.reset();
+});
